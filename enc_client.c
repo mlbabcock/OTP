33da24267e7fe956a16d49a3910e26d8422d0976
mlbabcock <91307276+mlbabcock@users.noreply.github.com>
2023-08-15 15:12:32 -0700

    Update enc_client.c updated main - server issues

--- enc_client.c
+++ enc_client.c
@@ -11,24 +11,24 @@
 
 int main() {
   int socket_fd;
-  char plaintext[1024];
+  char *plaintext;
   int plaintext_length;
-  char key[1024];
+  char *key;
   int key_length;
-  char ciphertext[1024];
+  char *ciphertext;
   int bytes_sent;
   int bytes_received;
 
   struct sockaddr_in address;
 
+  plaintext = malloc(1024 * sizeof(char));
   printf("Enter the plaintext: ");
-  fgets(plaintext, sizeof(plaintext), stdin);
+  fgets(plaintext, 1024, stdin);
   plaintext_length = strlen(plaintext);
-
+  key = malloc(1024 * sizeof(char));
   printf("Enter the key: ");
-  fgets(key, sizeof(key), stdin);
+  fgets(key, 1024, stdin);
   key_length = strlen(key);
-
   socket_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (socket_fd == -1) {
     perror("socket");
@@ -36,7 +36,7 @@ int main() {
   }
 
   address.sin_family = AF_INET;
-  address.sin_port = htons(57171); 
+  address.sin_port = htons(57171);
   address.sin_addr.s_addr = INADDR_ANY;
 
   int connect_result = connect(socket_fd, (struct sockaddr *)&address, sizeof(address));
@@ -44,6 +44,7 @@ int main() {
     perror("connect");
     exit(1);
   }
+
   bytes_sent = send(socket_fd, plaintext, plaintext_length, 0);
   if (bytes_sent == -1) {
     perror("send");
@@ -54,17 +55,19 @@ int main() {
     perror("send");
     exit(1);
   }
-
-  bytes_received = recv(socket_fd, ciphertext, sizeof(ciphertext), 0);
+  ciphertext = malloc(1024 * sizeof(char));
+  bytes_received = recv(socket_fd, ciphertext, 1024, 0);
   if (bytes_received == -1) {
     perror("recv");
     exit(1);
   }
 
   close(socket_fd);
-
-  printf("The ciphertext is: ");
-  fwrite(ciphertext, 1, bytes_received, stdout);
+  printf("The ciphertext is: %s\n", ciphertext);
+  
+  free(plaintext);
+  free(key);
+  free(ciphertext);
 
   return 0;
 }
