c8d528efed390ad26120c1d70e6c0a0ce889903f
mlbabcock <91307276+mlbabcock@users.noreply.github.com>
2023-08-14 22:03:18 -0700

    Update keygen.c updated

--- keygen.c
+++ keygen.c
@@ -5,7 +5,7 @@
 /* Function to generate a random character from the allowed set */
 char getRandomCharacter() {
     const char allowed_characters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-    int num_characters = sizeof(allowed_characters) - 1;            // Exclude null terminator
+    int num_characters = sizeof(allowed_characters) - 1; // Exclude null terminator
     return allowed_characters[rand() % num_characters];
 }
 
@@ -16,13 +16,13 @@ int main(int argc, char *argv[]) {
     }
 
     int key_length = atoi(argv[1]);
-    srand(time(NULL));                                              // Seed the random number generator
+    srand(time(NULL));                                          // Seed the random number generator
 
-    for (int i = 0; i < key_length; i++) {                         // Generate and output the key
+    for (int i = 0; i < key_length; i++) {
         char random_char = getRandomCharacter();
         putchar(random_char);
     }
 
-    putchar('\n');                                                 // Output a newline at the end
+    putchar('\n');                                              // Output a newline at the end
     return 0;
 }
