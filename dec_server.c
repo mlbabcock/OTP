92afd9b0cc61bde89d331804253f2785d74b3ab4
mlbabcock <91307276+mlbabcock@users.noreply.github.com>
2023-08-15 10:57:18 -0700

    Update dec_server.c updated. ode

--- dec_server.c
+++ dec_server.c
@@ -1,108 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
-/* Function to perform decryption based on given key and ciphertext */
-void performDecryption(char *ciphertext, char *key, char *plaintext) {
-    int ciphertext_length = strlen(ciphertext);
-    int key_length = strlen(key);
-
-    for (int i = 0; i < ciphertext_length; i++) {
-        char ciphertext_char = ciphertext[i];
-
-        char key_char = key[i % key_length];                                           // Repeating key
-
-        if (ciphertext_char == ' ') {
-            plaintext[i] = ' ';
-        } else {
-            int ciphertext_val = ciphertext_char - 'A'; 
-            int key_val = key_char - 'A';                                               // Convert to 0-25 range
-            int decrypted_val = (ciphertext_val - key_val) % 27;
-            
-            if (decrypted_val < 0) {                                                    // Handle negative values due to modulo
-                decrypted_val += 27;
-            }
-            plaintext[i] = decrypted_val + 'A';                                         // Convert back to ASCII range
-        }
-    }
-    plaintext[ciphertext_length] = '\0'; 
-}
-
+#include <netdb.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s listening_port\n", argv[0]);
-        return 1;
+  if (argc != 2) {
+    fprintf(stderr, "Usage: dec_server listening_port\n");
+    exit(1);
+  }
+
+  int listening_port = atoi(argv[1]);
+  int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_fd == -1) {
+    perror("socket");
+    exit(1);
+  }
+
+  struct sockaddr_in address;
+  memset(&address, 0, sizeof(address));
+  address.sin_family = AF_INET;
+  address.sin_port = htons(listening_port);
+  address.sin_addr.s_addr = INADDR_ANY;
+  int bind_result = bind(socket_fd, (struct sockaddr *)&address, sizeof(address));
+  if (bind_result == -1) {
+    perror("bind");
+    exit(1);
+  }
+  listen(socket_fd, 5);
+
+  while (1) {
+    int client_fd = accept(socket_fd, NULL, NULL);
+    if (client_fd == -1) {
+      perror("accept");
+      continue;
     }
 
-    int listening_port = atoi(argv[1]);
+    pid_t pid = fork();
+    if (pid == 0) {
+      close(socket_fd);
+      char ciphertext[1024];
+      char key[1024];
+      int bytes_received = recv(client_fd, ciphertext, sizeof(ciphertext), 0);
+      if (bytes_received == -1) {
+        perror("recv");
+        exit(1);
+      }
+      bytes_received = recv(client_fd, key, sizeof(key), 0);
+      if (bytes_received == -1) {
+        perror("recv");
+        exit(1);
+      }
+      char plaintext[1024];
+      for (int i = 0; i < bytes_received; i++) {
+        plaintext[i] = ciphertext[i] ^ key[i];
+      }
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);                                           // Create socket
-    if (sockfd == -1) {
-        perror("Error creating socket");
-        return 1;
+      bytes_sent = send(client_fd, plaintext, sizeof(plaintext), 0);
+      if (bytes_sent == -1) {
+        perror("send");
+        exit(1);
+      }
+      close(client_fd);
+      exit(0);
+    } else if (pid < 0) {
+      perror("fork");
+      continue;
     }
-
-    struct sockaddr_in server_addr;                                                        // Set up sockaddr_in structure
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(listening_port);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {        // Bind socket to the specified port
-        perror("Error binding");
-        return 1;
-    }
-
-    if (listen(sockfd, 5) == -1) {                                                        // Listen for incoming connections
-        perror("Error listening");
-        return 1;
-    }
-
-    while (1) {                                                                             // Accept connections and serve clients
-        struct sockaddr_in client_addr; 
-        socklen_t client_len = sizeof(client_addr);
-        int client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
-        if (client_sockfd == -1) {
-            perror("Error accepting connection");
-            continue;
-        }
-
-        pid_t child_pid = fork();
-        if (child_pid == -1) {
-            perror("Error forking");
-            close(client_sockfd);
-            continue;
-        }
-
-        if (child_pid == 0) { 
-            close(sockfd);                                                                 // Read ciphertext and key from the client
-            char ciphertext[512];
-            char key[512];
-            ssize_t ciphertext_len = recv(client_sockfd, ciphertext, sizeof(ciphertext), 0);
-            ssize_t key_len = recv(client_sockfd, key, sizeof(key), 0);
-            if (ciphertext_len == -1 || key_len == -1) {
-                perror("Error receiving data");
-                close(client_sockfd);
-                exit(1);
-            }
-
-            char plaintext[512];
-            performDecryption(ciphertext, key, plaintext);
-            ssize_t sent_bytes = send(client_sockfd, plaintext, strlen(plaintext), 0);
-            if (sent_bytes == -1) {
-                perror("Error sending data");
-                close(client_sockfd);
-                exit(1);
-            }
-            close(client_sockfd);                                                     // Close the client socket and exit the child process
-            exit(0);
-        } else { 
-            close(client_sockfd);
-        }
-    }
-    close(sockfd);
-    return 0;
+  }
+  return 0;
 }
