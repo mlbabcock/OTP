f6a742d7bf3625a3bde889a0acc036de6d173102
mlbabcock <91307276+mlbabcock@users.noreply.github.com>
2023-08-15 14:39:36 -0700

    Update dec_client.c trying to fix issues

--- dec_client.c
+++ dec_client.c
@@ -9,67 +9,42 @@
 #include <errno.h>
 #include <sys/socket.h>
 
-int main(int argc, char *argv[]) {
-  if (argc != 3) {
-    fprintf(stderr, "Usage: dec_client ciphertext key port\n");
-    exit(1);
-  }
-
-  char *ciphertext_file_name = argv[1];
-  char *key_file_name = argv[2];
-  int port_number = atoi(argv[3]);
-	FILE *ciphertext_file = fopen("ciphertext.txt", "r");
-  if (ciphertext_file == NULL) {
-    fprintf(stderr, "Could not open ciphertext file: %s\n", ciphertext_file_name);
-    exit(1);
-  }
-
-	FILE *key_file = fopen("key.txt", "r");
-  if (key_file == NULL) {
-    fprintf(stderr, "Could not open key file: %s\n", key_file_name);
-    exit(1);
-  }
-
-  char ciphertext[1024];
-  int ciphertext_length = fread(ciphertext, 1, sizeof(ciphertext), ciphertext_file);
-  if (ciphertext_length == -1) {
-    perror("fread");
-    exit(1);
-  }
-	fclose(ciphertext_file);
-
-  char key[1024];
-  int key_length = fread(key, 1, sizeof(key), key_file);
-  if (key_length == -1) {
-    perror("fread");
-    exit(1);
-  }
-	fclose(key_file);
+int main() {
+  int socket_fd;
+  char *ciphertext;
+  int ciphertext_length;
+  char *key;
+  int key_length;
+  char *plaintext;
+  int bytes_sent;
+  int bytes_received;
 
   struct sockaddr_in address;
-  memset(&address, 0, sizeof(address));
-  address.sin_family = AF_INET;
-  address.sin_port = htons(port_number);
-  address.sin_addr.s_addr = INADDR_ANY;
-  int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-
+	
+  ciphertext = malloc(1024 * sizeof(char));
+  printf("Enter the ciphertext: ");
+  fgets(ciphertext, 1024, stdin);
+  ciphertext_length = strlen(ciphertext);
+  key = malloc(1024 * sizeof(char));
+  printf("Enter the key: ");
+  fgets(key, 1024, stdin);
+  key_length = strlen(key);
+  socket_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (socket_fd == -1) {
     perror("socket");
     exit(1);
   }
-
+	
+  address.sin_family = AF_INET;
+  address.sin_port = htons(57171);
+  address.sin_addr.s_addr = INADDR_ANY;
   int connect_result = connect(socket_fd, (struct sockaddr *)&address, sizeof(address));
   if (connect_result == -1) {
-    if (errno == ECONNREFUSED) {
-      fprintf(stderr, "Could not connect to dec_server on port %d\n", port_number);
-      exit(2);
-    } else {
-      perror("connect");
-      exit(1);
-    }
+    perror("connect");
+    exit(1);
   }
 
-  int bytes_sent = send(socket_fd, ciphertext, ciphertext_length, 0);
+  bytes_sent = send(socket_fd, ciphertext, ciphertext_length, 0);
   if (bytes_sent == -1) {
     perror("send");
     exit(1);
@@ -79,14 +54,21 @@ int main(int argc, char *argv[]) {
     perror("send");
     exit(1);
   }
-
-  char plaintext[1024];
-  int bytes_received = recv(socket_fd, plaintext, sizeof(plaintext), 0);
+  plaintext = malloc(1024 * sizeof(char));
+  bytes_received = recv(socket_fd, plaintext, 1024, 0);
   if (bytes_received == -1) {
     perror("recv");
     exit(1);
   }
   close(socket_fd);
-  printf("%s\n", plaintext);
+	
+  FILE *fp = fopen("plaintext1_a", "w");
+  fputs(plaintext, fp);
+  fclose(fp);
+
+  free(ciphertext);
+  free(key);
+  free(plaintext);
+
   return 0;
 }
