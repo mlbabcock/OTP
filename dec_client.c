ea247a37881b7db2c0f7071fe3ebfd84aad046a5
mlbabcock <91307276+mlbabcock@users.noreply.github.com>
2023-08-15 10:59:51 -0700

    Update dec_client.c updated code

--- dec_client.c
+++ dec_client.c
@@ -1,109 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
+#include <string.h>
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include <stdbool.h>
-#include <fcntl.h>
-#include <ctype.h>
-
-#define MAX 9000000
-
-void error(const char *message){
-    perror(message);
-    exit(1);
-}
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s ciphertext key port\n", argv[0]);
-        return 1;
-    }
+  if (argc != 3) {
+    fprintf(stderr, "Usage: dec_client ciphertext key port\n");
+    exit(1);
+  }
 
-    char *ciphertext_filename = argv[1];
-    char *key_filename = argv[2];
-    int port = atoi(argv[3]);
+  char *ciphertext_file_name = argv[1];
+  char *key_file_name = argv[2];
+  int port_number = atoi(argv[3]);
+  FILE *ciphertext_file = fopen(ciphertext_file_name, "r");
+  if (ciphertext_file == NULL) {
+    fprintf(stderr, "Could not open ciphertext file: %s\n", ciphertext_file_name);
+    exit(1);
+  }
 
-    FILE *ciphertext_file = fopen(ciphertext_filename, "r");                                // Read ciphertext from file
-    if (!ciphertext_file) {
-        perror("Error opening ciphertext file");
-        return 1;
-    }
-    char ciphertext[512];
-    if (fgets(ciphertext, sizeof(ciphertext), ciphertext_file) == NULL) {
-        perror("Error reading ciphertext");
-        fclose(ciphertext_file);
-        return 1;
-    }
+  FILE *key_file = fopen(key_file_name, "r");
+  if (key_file == NULL) {
+    fprintf(stderr, "Could not open key file: %s\n", key_file_name);
+    exit(1);
+  }
 
-    // Remove newline characters at the end of the ciphertext if present
-    size_t ciphertext_length = strlen(ciphertext);
-    if (ciphertext_length > 0 && ciphertext[ciphertext_length - 1] == '\n') {
-        ciphertext[ciphertext_length - 1] = '\0';
-    }
-    fclose(ciphertext_file);
+  char ciphertext[1024];
+  int ciphertext_length = fread(ciphertext, 1, sizeof(ciphertext), ciphertext_file);
+  if (ciphertext_length == -1) {
+    perror("fread");
+    exit(1);
+  }
+  ciphertext_file.close();
 
-    FILE *key_file = fopen(key_filename, "r");                                              // Read key from file
-    if (!key_file) {    
-        perror("Error opening key file");
-        return 1;
-    }
-    char key[512];
-    if (fgets(key, sizeof(key), key_file) == NULL) {
-        perror("Error reading key");
-        fclose(key_file);
-        return 1;
-    }
+  char key[1024];
+  int key_length = fread(key, 1, sizeof(key), key_file);
+  if (key_length == -1) {
+    perror("fread");
+    exit(1);
+  }
+  key_file.close();
+  struct sockaddr_in address;
+  memset(&address, 0, sizeof(address));
+  address.sin_family = AF_INET;
+  address.sin_port = htons(port_number);
+  address.sin_addr.s_addr = INADDR_ANY;
+  int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Remove newline characters at the end of the key if present
-    size_t key_length = strlen(key);
-    if (key_length > 0 && key[key_length - 1] == '\n') {
-        key[key_length - 1] = '\0';
+  if (socket_fd == -1) {
+    perror("socket");
+    exit(1);
+  }
+
+  int connect_result = connect(socket_fd, (struct sockaddr *)&address, sizeof(address));
+  if (connect_result == -1) {
+    if (errno == ECONNREFUSED) {
+      fprintf(stderr, "Could not connect to dec_server on port %d\n", port_number);
+      exit(2);
+    } else {
+      perror("connect");
+      exit(1);
     }
-    fclose(key_file);
+  }
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);                                             // Create socket
-    if (sockfd == -1) {
-        perror("Error creating socket");
-        return 1;
-    }
+  int bytes_sent = send(socket_fd, ciphertext, ciphertext_length, 0);
+  if (bytes_sent == -1) {
+    perror("send");
+    exit(1);
+  }
+  bytes_sent = send(socket_fd, key, key_length, 0);
+  if (bytes_sent == -1) {
+    perror("send");
+    exit(1);
+  }
 
-    struct hostent *server = gethostbyname("localhost");
-    if (server == NULL) {
-        perror("Error resolving hostname");
-        return 1;
-    }
-
-    /* Set up sockaddr_in structure */
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
-
-    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {       // Connect to the server
-        perror("Error connecting to server");
-        return 2;
-    }
-
-    if (send(sockfd, ciphertext, strlen(ciphertext), 0) == -1 ||                           // Send ciphertext and key to server
-        send(sockfd, key, strlen(key), 0) == -1) {
-        perror("Error sending data to server");
-        close(sockfd);
-        return 1;
-    }
-
-    char plaintext[512];                                                                   // Receive plaintext from server
-    ssize_t plaintext_len = recv(sockfd, plaintext, sizeof(plaintext), 0);
-    if (plaintext_len == -1) {
-        perror("Error receiving plaintext");
-        close(sockfd);
-        return 1;
-    }
-
-    fwrite(plaintext, sizeof(char), plaintext_len, stdout);
-    close(sockfd);
-    return 0;
+  char plaintext[1024];
+  int bytes_received = recv(socket_fd, plaintext, sizeof(plaintext), 0);
+  if (bytes_received == -1) {
+    perror("recv");
+    exit(1);
+  }
+  close(socket_fd);
+  printf("%s\n", plaintext);
+  return 0;
 }
