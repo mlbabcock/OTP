c880cf71eb6f4894ae40001c9adcb901498beb7f
mlbabcock <91307276+mlbabcock@users.noreply.github.com>
2023-08-14 21:50:52 -0700

    Update keygen.c

--- keygen.c
+++ keygen.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+/* Function to generate a random character from the allowed set */
+char getRandomCharacter() {
+    const char allowed_characters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+    int num_characters = sizeof(allowed_characters) - 1;            // Exclude null terminator
+    return allowed_characters[rand() % num_characters];
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s keylength\n", argv[0]);
+        return 1;
+    }
+
+    int key_length = atoi(argv[1]);
+    srand(time(NULL));                                              // Seed the random number generator
+
+    for (int i = 0; i < key_length; i++) {                         // Generate and output the key
+        char random_char = getRandomCharacter();
+        putchar(random_char);
+    }
+
+    putchar('\n');                                                 // Output a newline at the end
+    return 0;
+}
